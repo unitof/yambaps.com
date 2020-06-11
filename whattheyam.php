4bf70552512e87155b91c9aa816d28e9c94e6adc
Jacob Ford <j@cobford.com>
2020-06-11 15:12:45 -0400

    Initial commit, though this was code Leapday 2012
    (based on a Weebl tweet)

---
+++ whattheyam.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<!--I always forget the HTML comment syntax, so I put this here for quick reference.-->
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="Keywords" content="yambaps, jacob ford, weebl">
+	<meta name="Description" content="So new! So hip!">
+	<title>Yambaps! It's quirky cool!</title>
+	<link rel="shortcut icon" href="images/favicon.png">
+	<link rel="apple-itouch-icon" href="images/touch.png">
+	<link rel="image_src" href="images/touch.png">
+	<link rel="stylesheet" type="text/css" href="yammy.css">
+
+	<?php include './includes/ga.php'; ?>
+</head>
+
+<body>
+	<h1><a href="/">Yambaps</a></h1>
+	<h3><a href="http://bit.ly/wOjrko">@theweebl</a> started it. <a href="http://jacobford.com">I</a> simply finished the matter.</h3>
+</body>
+</html>
